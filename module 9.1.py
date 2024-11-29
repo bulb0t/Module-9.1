e62832f6b300c3affb94e684cf9f98b929214769
bulb0t <yu.uglenko@yandex.ru>
2024-11-29 21:51:05 +0300

    Add files via upload

--- module 9.1.py
+++ module 9.1.py
@@ -1,8 +1,11 @@
 def apply_all_func(int_list, *functions):
-    results = {}
-    for fun in functions:
-        results[fun.__name__] = fun(int_list)
-    return results
+    try:
+        results = {}
+        for fun in functions:
+            results[fun.__name__] = fun(int_list)
+        return results
+    except TypeError as exc:
+        return f'В вашем списке содержится некорректный тип данных, из-за этого возникает ошибка: {exc}'
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
